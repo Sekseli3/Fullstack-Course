8fcccfb111fb07f60fa275b163bdece2641f6906
Sekseli3 <akseli.tuominen@aalto.fi>
2023-01-18 15:03:58 +0200

    Anecdote

--- osa1/Anecdote/App.js
+++ osa1/Anecdote/App.js
@@ -9,49 +9,65 @@ const App = () => {
   const anecdotes = [
     {
     text:'If it hurts, do it more often.',
-    number: 1
+    number: 0
   },
   {
     text:'Adding manpower to a late software project makes it later!',
-    number: 2
+    number: 0
   },
   {
     text:'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    number: 3
+    number: 0
   },
   {
     text:'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    number: 4
+    number: 0
   },
   {
     text:'Premature optimization is the root of all evil.',
-    number: 5
+    number: 0
   },
   {
     text:'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    number: 6
+    number: 0
   },
   {
     text: 'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
-    number: 7
+    number: 0
   },
   {
     text:'The only way to go fast, is to go well.',
-    number: 8
+    number: 0
   }
   ]
+
+  const [random, setRandom] = useState(0)
+  const [points, setPoints] = useState(new Array(8).fill(0))
+  
    
   const getAnecdote = () => {
-    const pos = Math.floor((Math.random()*8)+0)
-    setRandom(pos)
+    const num = Math.floor(Math.random()*8)
+    setRandom(num)
   }
-  const [random, setRandom] = useState(0)
+
+  
+    const addVote = () => {
+    const copy = [...points]
+    ++copy[random]
+    setPoints(copy)
+  }
+  const index = points.indexOf(Math.max(...points))
 
   return (
     <div>
-      <Button click={getAnecdote} text= "randomiser"/>
+      <Button click={getAnecdote} text= "next anecdote"/>
+      <Button click={addVote} text = "vote" />
+      <h2>Anecdote</h2>
       <p>{anecdotes[random].text}</p>
-      <h1>{anecdotes[0].text}</h1>
+      <h3>Votes</h3>
+      <p>{points[random]}</p>
+      <h3>Anecdote with most votes</h3>
+      <p>{anecdotes[index].text} with {points[index]} votes</p>
     </div>
   );
 

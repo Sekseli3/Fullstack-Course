2f368b45f610065c77326f4df50fbf870bae4c79
Sekseli3 <120391401+Sekseli3@users.noreply.github.com>
2023-02-18 09:30:12 +0200

    Small change to 4.23

--- osa4/utils/middleware.js
+++ osa4/utils/middleware.js
@@ -45,7 +45,7 @@ const tokenExtractor = (request, response, next) => {
 const userExtractor = async (request, response, next) => {
   const authorization = request.get('authorization')
   
-  if (authorization && authorization.startsWith('bearer ')) {
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
     const decodedToken = jwt.verify(authorization.substring(7), process.env.SECRET)
     if (decodedToken) {
       request.user = await User.findById(decodedToken.id)
